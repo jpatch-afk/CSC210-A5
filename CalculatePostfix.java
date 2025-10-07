d768d48719397e37ac9284cfe7974274b95dc88d
jpatch-afk <jpaczkowski@smith.edu>
2025-10-07 11:50:21 -0400

    Starting Calculator

--- CalculatePostfix.java
+++ CalculatePostfix.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+import java.io.StringReader;
+
 public class CalculatePostfix {
     public static Double postfixToResult(Queue<Object> tokens) {
-        // FILL IN
+        Queue<Object> queue = new Queue<>();
+
+        
+        
+
+
+
+
+
     }
 }
